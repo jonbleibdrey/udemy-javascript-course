4b42c8e1c16f81adb0154ccd4e9ea3a1c254b0ca
jonathan bleibdrey <jonbleibdrey@aol.com>
2022-02-07 15:28:51 -0500

    relearning javascript basics

---
+++ script.js
@@ -0,0 +1,48 @@
+// let js = "amazing";
+// // if (js === "amazing") alert("fun times bitches");
+
+// console.log(40 + 8 + 23 + 56);
+
+// let firstName = "Bob";
+// let first = "jonathan";
+
+// console.log(firstName);
+
+// let country = "america";
+// let continent = "north america";
+// let population = "1 billion";
+
+// const isIsland = country;
+// console.log(typeof true);
+
+// if (isIsland === "america"){
+//   console.log(true);
+// } else {
+//   console.log(false);
+// } ;
+
+// BMI = mass/height ** 2 = mass/ (height * height) (mass in kg and height in meter )
+
+// const markHeight = 1.69
+// const markWeight = 78
+// const johnWeight = 92
+// const johnHeight = 1.95
+
+// const markConversion = markWeight / markHeight ** 2
+// const johnConversion = johnWeight / johnHeight ** 2
+
+// if (markConversion > johnConversion){
+//   console.log("mark has a higher bmi")
+// } else {
+//   console.log("john has a higher bmi")
+// }
+
+// console.log(markConversion)
+
+const firstName = "Jonathan" ;
+const job = "teacher";
+
+const nameAndJob = "I'm " + firstName + " i am, an " + job ;
+
+console.log(nameAndJob)
+
